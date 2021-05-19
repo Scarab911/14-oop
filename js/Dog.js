bf8474700e4206404e98cb1fe1db8418c6a25e5d
Paulius B <Paulspremium@gmail.com>
2021-05-19 20:57:22 +0300

    Dog Class

---
+++ js/Dog.js
@@ -0,0 +1,10 @@
+class Dog {
+    constructor(name, fur){
+        this.name = name;
+        this.furColor = fur;
+        this.legsCount = 4;
+        this.hasTail = true;
+    }
+}
+
+module.exports = Dog;
